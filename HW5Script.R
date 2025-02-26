653f28d31aa5f2f44ddd0e91cf921a36ef45d710
dannymolyneux <dmolyneux@colgate.edu>
2025-02-26 00:01:55 -0500

    Step 2
    Made a data frame containing features for every json song

--- HW5Script.R
+++ HW5Script.R
@@ -18,8 +18,8 @@ track.artist = str_split(artist, "/")[[1]][[2]] #just artist name
 #track.name
 #track.album
 #track.album
-json.r = fromJSON(current.filename)
-features = tibble(
+json.r = fromJSON(current.filename) #processes the json file
+features = tibble( #data frame containing each feature for the json file
   overall_loudness = json.r$lowlevel$loudness_ebu128$integrated,
   spectral_energy = json.r$lowlevel$spectral_energy,
   dissonance = json.r$lowlevel$dissonance,
@@ -30,5 +30,44 @@ features = tibble(
   tuning_frequency = json.r$tonal$tuning_frequency
 )
 #view(features)
+
+#step 2
+files = list.files("EssentiaOutput", pattern = ".json") #gets a list of all files from EssentiaOutput
+#lapply will apply all of these methods to each file in files
+json.df <- lapply(files, function(file) { #data frame that will have info for each song
+  json <- fromJSON(paste("EssentiaOutput/", file, sep = "")) #variable for each new file
+  file.parts <- str_split(basename(file), "-")[[1]] #gives just the file parts, not the "EssentiaOutput/"
+  track.json <- file.parts[length(file.parts)] #track name with .json
+  track <- str_sub(track.json, start = 1, end = length(track.name) - 7) #just track name
+  album <- file.parts[length(file.parts) - 1] #album name
+  artist <- file.parts[length(file.parts) - 2] #artist name
+  #features
+  overall_loudness <- json$lowlevel$loudness_ebu128$integrated
+  spectral_energy <- json$lowlevel$spectral_energy$mean
+  dissonance <- json$lowlevel$dissonance$mean
+  pitch_salience <- json$lowlevel$pitch_salience$mean
+  bpm <- json$rhythm$bpm
+  beats_loudness <- json$rhythm$beats_loudness$mean
+  danceability <- json$rhythm$danceability
+  tuning_frequency <- json$tonal$tuning_frequency
+  #tibble to make sure we only have columns for the relevant variables
+  tibble(
+    track,
+    album,
+    artist,
+    overall_loudness,
+    spectral_energy,
+    dissonance,
+    pitch_salience,
+    bpm,
+    beats_loudness,
+    danceability,
+    tuning_frequency,
+  )
+})
+json.df = bind_rows(json.df)
+view(json.df)
+
+
 
 
